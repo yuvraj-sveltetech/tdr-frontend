a81f06d79edf4694cd6abec0bed85358394de116
yuvraj-sveltetech <yuvraj@sveltetech.com>
2023-05-11 15:42:58 +0530

    chart done

--- src/components/utils/ConstantUrl.jsx
+++ src/components/utils/ConstantUrl.jsx
@@ -4,3 +4,5 @@ export const CREATE_FOLDER = "tdr/tdrFolder/";
 export const GET_HEADERS = "tdr/getSubFolder/";
 export const GET_EXCEL_DATA = "tdr/getUserExcelData/";
 export const GET_EXCEL_PAGINATION = "tdr/getResultData/";
+export const VOIP_GRAPH = "/tdr/VoipDiagramData/";
+export const SINGLE_NUMBER_CHART_DATA = "/tdr/getSingleVoipChart/";
