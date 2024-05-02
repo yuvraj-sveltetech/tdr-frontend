f330afe2f71f2f32f76ea98e301ea13583e4b7bb
yuvraj-sveltetech <yuvraj@sveltetech.com>
2024-05-02 18:14:10 +0530

    login

--- src/components/routes/AppRouter.jsx
+++ src/components/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import {
@@ -6,6 +6,7 @@ import {
   Routes,
   BrowserRouter as Router,
   Navigate,
+  useNavigate,
 } from "react-router-dom";
 import {
   Login,
@@ -19,8 +20,20 @@ import {
 } from "../utils/index";
 import PrivateRoute from "../utils/PrivateRoute";
 import Layout from "../home/layout/Layout";
+import Cookies from "js-cookie";
 
 const AppRouter = () => {
+  const isAuth = Cookies.get("ss_tkn");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuth) {
+      navigate("/");
+    } else {
+      navigate("/dashboard");
+    }
+  }, [isAuth]);
+
   return (
     <Router>
       <ToastContainer />
@@ -28,13 +41,13 @@ const AppRouter = () => {
         <Routes>
           <Route path="/" index element={<Login />} />
           <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/:parent_folder" element={<SubFolder />} />
-          <Route path="/:parent_folder/:subfolder" element={<DirFiles />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="/user-log" element={<UserLog />} />
-          <Route path="/upload-sdr" element={<UploadSdr />} />
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/:parent_folder" element={<SubFolder />} />
+            <Route path="/:parent_folder/:subfolder" element={<DirFiles />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="/user-log" element={<UserLog />} />
+            <Route path="/upload-sdr" element={<UploadSdr />} />
           </Route>
           <Route path="/not-found" element={<PageNotFound />} />
           <Route path="*" element={<Navigate to="/not-found" replace />} />
