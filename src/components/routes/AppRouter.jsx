b9c650f76278e65290e6846f338c0475bd1bc627
yuvraj-sveltetech <yuvraj@sveltetech.com>
2024-05-02 16:47:38 +0530

    routing changed

--- src/components/routes/AppRouter.jsx
+++ src/components/routes/AppRouter.jsx
@@ -28,12 +28,13 @@ const AppRouter = () => {
         <Routes>
           {/* <Route path="/" index element={<Login />} /> */}
           {/* <Route element={<PrivateRoute />}> */}
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/:parent_folder" element={<SubFolder />} />
-            <Route path="/:parent_folder/:subfolder" element={<DirFiles />} />
-            <Route path="/report" element={<Report />} />
-            <Route path="/user-log" element={<UserLog />} />
-            <Route path="/upload-sdr" element={<UploadSdr />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/:parent_folder" element={<SubFolder />} />
+          <Route path="/:parent_folder/:subfolder" element={<DirFiles />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/user-log" element={<UserLog />} />
+          <Route path="/upload-sdr" element={<UploadSdr />} />
           {/* </Route> */}
           <Route path="/not-found" element={<PageNotFound />} />
           <Route path="*" element={<Navigate to="/not-found" replace />} />
