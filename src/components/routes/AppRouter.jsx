b24b5a0db0a3a1345872af2377be0c2b954b2300
yuvraj-sveltetech <yuvraj@sveltetech.com>
2024-05-07 16:43:20 +0530

    changes

--- src/components/routes/AppRouter.jsx
+++ src/components/routes/AppRouter.jsx
@@ -37,7 +37,6 @@ const AppRouter = () => {
             <Route path="/report" element={<Report />} />
             <Route path="/user-log" element={<UserLog />} />
             <Route path="/upload-sdr" element={<UploadSdr />} />
-            <Route path="/test/:a/:b" element={<h1>Test</h1>} />
           </Route>
           <Route path="/not-found" element={<PageNotFound />} />
           <Route path="*" element={<Navigate to="/not-found" replace />} />
