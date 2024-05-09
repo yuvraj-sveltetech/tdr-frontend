3ced4110377c432a5614a986d7f23300140c534e
yuvraj-sveltetech <yuvraj@sveltetech.com>
2024-05-09 10:56:10 +0530

    ui fix and select all btn on sub folder

--- src/components/home/dashboard/SubFolder.jsx
+++ src/components/home/dashboard/SubFolder.jsx
@@ -114,13 +114,11 @@ const SubFolder = ({ toggleFileUploadModal, category, modalType }) => {
         />
         <div className="sub_folder d-flex flex-column align-items-start">
           {/* <h5>{files.sub_folders?.parent_folder}</h5> */}
-          <div
-            className="all-folders container"
-            style={{ overflow: "auto", height: "60vh" }}
-          >
+          <div className="all-folders container" style={{ height: "60vh" }}>
             <hr style={{ padding: "0", margin: "0", color: "#B6B6B6" }} />
             <div className="d-flex align-items-center justify-content-between">
               <h6>FOLDER</h6>
+
               <div className="d-flex align-items-center">
                 <label
                   htmlFor="select_All"
@@ -145,69 +143,139 @@ const SubFolder = ({ toggleFileUploadModal, category, modalType }) => {
               </div>
             </div>
 
-            {loading && subFolder?.subFolder?.length === 0 ? (
-              <div className="d-flex justify-content-center center-div">
-                <div className="spinner-border" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
-              </div>
-            ) : subFolder?.subFolder?.length > 0 ? (
-              <div className="row list-unstyled">
-                {subFolder?.subFolder?.map((folder) => {
-                  return (
-                    <div
-                      className="col-md-3 mb-3"
-                      key={`SubFolder${folder?.id}`}
-                    >
-                      <div
-                        className="folder d-flex justify-content-start align-items-center subfolder-box position-relative"
-                        onClick={(e) =>
-                          navigate(`/${subFolder?.id}/${folder?.id}`)
-                        }
-                      >
-                        <span
-                          className="align-self-end me-2 position-absolute cursot"
-                          style={{ bottom: "5px", right: "-5px" }}
-                        >
-                          {/* <MdOutlineFileDownload
-                            size={20}
-                            color="gray"
-                            onClick={(e) => exportCSV(e, folder?.id)}
-                          /> */}
-                        </span>
-
-                        <input
-                          type="checkbox"
-                          checked={isSubfolderChecked(folder?.id)}
-                          onChange={(e) => selectAllFiles(e, folder)}
-                          onClick={(e) => e.stopPropagation()}
-                          className="align-self-end me-2 position-absolute end-0 cursot"
-                          style={{ top: "8px", cursor: "pointer" }}
-                        />
-                        <li
-                          onClick={(e) =>
-                            navigate(`/${subFolder?.id}/${folder?.id}`)
-                          }
-                        >
-                          <MdFolder size="32" className="folderIcon" />
-                        </li>
-                        <p style={{ margin: "auto 0", padding: "0 0.4rem" }}>
-                          {folder?.sub_folder_name}
-                        </p>
-                      </div>
+            <div
+              className="container"
+              style={{ overflow: "auto", height: "60vh" }}
+            >
+              <div className="row" style={{ height: "auto" }}>
+                {loading && subFolder?.subFolder?.length === 0 ? (
+                  <div className="d-flex justify-content-center center-div">
+                    <div className="spinner-border" role="status">
+                      <span className="visually-hidden">Loading...</span>
                     </div>
-                  );
-                })}
+                  </div>
+                ) : subFolder?.subFolder?.length > 0 ? (
+                  <>
+                    {subFolder?.subFolder?.map((folder) => {
+                      return (
+                        <div
+                          className="col-md-3"
+                          key={`SubFolder${folder?.id}`}
+                        >
+                          <div
+                            className="folder d-flex justify-content-start align-items-center subfolder-box position-relative list-unstyled mb-3"
+                            onClick={(e) =>
+                              navigate(`/${subFolder?.id}/${folder?.id}`)
+                            }
+                          >
+                            <span
+                              className="align-self-end me-2 position-absolute cursot"
+                              style={{ bottom: "5px", right: "-5px" }}
+                            ></span>
+
+                            <input
+                              type="checkbox"
+                              checked={isSubfolderChecked(folder?.id)}
+                              onChange={(e) => selectAllFiles(e, folder)}
+                              onClick={(e) => e.stopPropagation()}
+                              className="align-self-end me-2 position-absolute end-0 cursot"
+                              style={{ top: "8px", cursor: "pointer" }}
+                            />
+                            <li
+                              onClick={(e) =>
+                                navigate(`/${subFolder?.id}/${folder?.id}`)
+                              }
+                            >
+                              <MdFolder size="32" className="folderIcon" />
+                            </li>
+                            <p
+                              style={{ margin: "auto 0", padding: "0 0.4rem" }}
+                            >
+                              {folder?.sub_folder_name}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </>
+                ) : (
+                  !loading && (
+                    <div className="center-div">
+                      <h6 style={{ color: "red" }}>
+                        Folder does not exist. Please create one
+                      </h6>
+                    </div>
+                  )
+                )}
               </div>
-            ) : (
-              !loading && (
-                <div className="center-div">
-                  <h6 style={{ color: "red" }}>
-                    Folder does not exist. Please create one
-                  </h6>
-                </div>
-              )
-            )}
+            </div>
+
+            {/* <div
+              className="container"
+              style={{ overflow: "auto", height: "60vh" }}
+            >
+              <div className="row">
+                {loading && subFolder?.subFolder?.length === 0 ? (
+                  <div className="d-flex justify-content-center center-div">
+                    <div className="spinner-border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </div>
+                  </div>
+                ) : subFolder?.subFolder?.length > 0 ? (
+                  <>
+                    {subFolder?.subFolder?.map((folder) => {
+                      return (
+                        <div
+                          className="col-md-3"
+                          key={`SubFolder${folder?.id}`}
+                        >
+                          <div
+                            className="folder d-flex justify-content-start align-items-center subfolder-box position-relative"
+                            onClick={(e) =>
+                              navigate(`/${subFolder?.id}/${folder?.id}`)
+                            }
+                          >
+                            <span
+                              className="align-self-end me-2 position-absolute cursot"
+                              style={{ bottom: "5px", right: "-5px" }}
+                            ></span>
+
+                            <input
+                              type="checkbox"
+                              checked={isSubfolderChecked(folder?.id)}
+                              onChange={(e) => selectAllFiles(e, folder)}
+                              onClick={(e) => e.stopPropagation()}
+                              className="align-self-end me-2 position-absolute end-0 cursot"
+                              style={{ top: "8px", cursor: "pointer" }}
+                            />
+                            <li
+                              onClick={(e) =>
+                                navigate(`/${subFolder?.id}/${folder?.id}`)
+                              }
+                            >
+                              <MdFolder size="32" className="folderIcon" />
+                            </li>
+                            <p
+                              style={{ margin: "auto 0", padding: "0 0.4rem" }}
+                            >
+                              {folder?.sub_folder_name}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </>
+                ) : (
+                  !loading && (
+                    <div className="center-div">
+                      <h6 style={{ color: "red" }}>
+                        Folder does not exist. Please create one
+                      </h6>
+                    </div>
+                  )
+                )}
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
