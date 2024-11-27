dc25add10fef0f02227330496df31adacd705ad9
Yuvraj <106510471+yuvraj-sveltetech@users.noreply.github.com>
2024-11-27 14:26:10 +0530

    search table data

--- src/components/home/search/SearchTable.jsx
+++ src/components/home/search/SearchTable.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { IoNavigateCircleOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const SearchTable = ({ data }) => {
+  const navigate = useNavigate();
+
   const extractFileName = (url) => {
     if (url?.length > 0) {
       console.log(url);
@@ -17,22 +21,34 @@ const SearchTable = ({ data }) => {
         >
           <tr>
             <th scope="col">#</th>
-            <th scope="col">File Name</th>
+            <th>Project ID</th>
+            <th>Location Folder Name</th>
+            {/* <th scope="col">File Name</th>
             <th scope="col">Date & Time</th>
-            <th scope="col">Location</th>
+            <th scope="col">Location</th> */}
           </tr>
         </thead>
         <tbody>
           {data?.map((item, i) => (
             <tr key={item?.file_id}>
               <th scope="row">{i + 1}</th>
-              <td>{extractFileName(item?.file_name)}</td>
+              <td>{item?.project}</td>
+              <td>{item?.location_name}</td>
+              {/* <td>
+                <span onClick={() => navigate(`/${item?.project}/${item?.id}`)}>
+                  <IoNavigateCircleOutline
+                    size={20}
+                    style={{ cursor: "pointer" }}
+                  />
+                </span>
+              </td> */}
+              {/* <td>{extractFileName(item?.file_name)}</td>
               <td>
                 {item?.time?.split("T")?.[0] +
                   " | " +
                   item?.time?.split("T")?.[1]}
               </td>
-              <td>{item?.project + " / " + item?.location}</td>
+              <td>{item?.project + " / " + item?.location}</td> */}
             </tr>
           ))}
         </tbody>
